83ebe5b4effd7d39b46a397edd324d77776feae4
shikhar1401 <shikharagarwal0717@gmail.com>
2020-07-18 22:04:02 +0530

    landing home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // {
-  //   path: "",
-  //   redirectTo: 'landing',
-  //   pathMatch: 'full'
-  // },
+  {
+    path: "",
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
   // {
   //   path: 'landing',
   //   loadChildren: () => import('./pages/landing/landing.module').then( m => m.LandingPageModule)
