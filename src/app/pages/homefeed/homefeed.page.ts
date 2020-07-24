644d11d638af5e80ce00e878f2ae8fd52d5b68aa
shikhar1401 <shikharagarwal0717@gmail.com>
2020-07-24 12:58:13 +0530

    console errror resolved

--- src/app/pages/homefeed/homefeed.page.ts
+++ src/app/pages/homefeed/homefeed.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homefeed',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomefeedPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { 
+    // this.router.events.subscribe((res) =>{
+    //   console.log(res);
+    //   console.log(this.router.url);
+    // })
+  }
 
   ngOnInit() {
+  }
+  clicktab(event: Event,tab: string){
+    event.stopImmediatePropagation();
   }
 
 }
--- src/app/pages/trades/trades.page.ts
+++ src/app/pages/trades/trades.page.ts
@@ -12,4 +12,8 @@ export class TradesPage implements OnInit {
   ngOnInit() {
   }
 
+  clickTab(event: Event, tab: string) {
+    event.stopImmediatePropagation();
+  }
+
 }
